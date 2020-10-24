40993811739adcc651cb2a03a23ffdd01a433284
hanhle95 <70605689+hanhle95@users.noreply.github.com>
2020-10-24 16:15:29 -0400

    Update home.php

--- webPages/home.php
+++ webPages/home.php
@@ -25,11 +25,11 @@
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css"
     />
-    <link rel="stylesheet" href="personal.css" />
+    <link rel="stylesheet" href="home.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
-    <script src="personal.js"></script>
+    <script src="home.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/js/all.min.js"></script>
   </head>
   <body>
